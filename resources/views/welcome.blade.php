d25ffab374c415206daabac67e830d8d4b726577
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-05-10 17:12:06 +0200

    [Working] Printed all the infos + Some style with Bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-@foreach ($trains as $train)
-    <h1>{{$train->enterprise}}</h1>
-@endforeach
+    <main class="bg-danger bg-opacity-50 py-3">
+        <div class="container bg-success  py-3">
+            <div class="row g-2">
+                @foreach ($trains as $train)
+                    <div class="col">
+                        <div class="card border-0" style="width: 15rem;">
+                            <div class="card-header bg-danger bg-opacity-50">
+                                <h1 class="">{{ $train->enterprise }}</h1>
+                                <h6>{{ $train->train_code }}</h4>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Stazione di partenza: <span
+                                        class="fw-bold">{{ $train->departure_station }}</span></li>
+                                <li class="list-group-item">Stazione di arrivo: <span
+                                        class="fw-bold">{{ $train->arrival_station }}</span></li>
+                                <li class="list-group-item {{ $train->is_in_time == 1 ? 'text-success' : 'text-danger' }}"> <span class="fw-bold">
+                                        {{ $train->is_in_time == 1 ? 'In orario' : 'È in ritardo' }}
+                                    </span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </main>
 @endsection
